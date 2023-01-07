2e42a42c76d079a6d4eb17f307d284dba9b0c7fd
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-01-07 07:07:12 +0200

    Completed the Sidebar with its Sass Styles!

--- procrew_frontend/src/Components/Header/Header.jsx
+++ procrew_frontend/src/Components/Header/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import "../Sass/Header/Header.css";
 
 export const Header = () => {
+  //   Opening & Closing Sidebar
+  const [sideNavDisplay, setSideNavDisplay] = useState("none");
+
+  // Automatically closes sidebar when width is greater than 1000px;  
+  setInterval(() => {
+    if (sideNavDisplay == "flex" && window.innerWidth > 1000) {
+        setSideNavDisplay("none");
+    }
+  }, 1);
+
   return (
     <header>
         <nav id="main-nav">
@@ -18,7 +29,20 @@ export const Header = () => {
                     <button>Log In</button>
                     <button>Sign Up</button>
                 </div>
-                <i className="fa-solid fa-burger"></i>
+                <i id="menu" className="fa-solid fa-burger" onClick={() => {sideNavDisplay == "none" ? setSideNavDisplay("flex") : setSideNavDisplay("none")}}></i>
+            </div>
+        </nav>
+        <nav id="side-nav" style={{display: `${sideNavDisplay}`}}>
+            <div id="side-nav-container">
+                <div id="side-nav-top">
+                    <a href="#">Home</a>
+                    <a href="#">Join</a>
+                    <a href="#">About</a>
+                </div>
+                <div id="side-nav-bottom">
+                    <button>Log In</button>
+                    <button>Sign Up</button>
+                </div>
             </div>
         </nav>
     </header>
