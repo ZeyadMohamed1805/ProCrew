9e7224153cff4294f4a3b29fe632b07c5ce09acb
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-01-09 06:57:12 +0200

    Fetched the menus data to the dashboard

--- procrew/procrew_frontend/src/Pages/Dashboard/Components/Restaurants/Restaurants.jsx
+++ procrew/procrew_frontend/src/Pages/Dashboard/Components/Restaurants/Restaurants.jsx
@@ -1,49 +1,32 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import "../Sass/Restaurants/Restaurants.css";
 
 export const Restaurants = () => {
-  const [loadCounter, setLoadCounter] = useState(8);
+  const [menusAPI, setMenusAPI] = useState([]);
 
-  const restaurants = [
-    {id: 0, icon: "icon0", title: "title0"},
-    {id: 1, icon: "icon1", title: "title1"},
-    {id: 2, icon: "icon2", title: "title2"},
-    {id: 3, icon: "icon3", title: "title3"},
-    {id: 4, icon: "icon4", title: "title4"},
-    {id: 5, icon: "icon5", title: "title5"},
-    {id: 6, icon: "icon6", title: "title6"},
-    {id: 7, icon: "icon7", title: "title7"},
-    {id: 8, icon: "icon8", title: "title8"},
-    {id: 9, icon: "icon9", title: "title9"},
-    {id: 10, icon: "icon10", title: "title10"},
-    {id: 11, icon: "icon11", title: "title11"},
-    {id: 12, icon: "icon12", title: "title12"},
-    {id: 13, icon: "icon13", title: "title13"},
-    {id: 14, icon: "icon14", title: "title14"},
-    {id: 15, icon: "icon15", title: "title15"},
-    {id: 16, icon: "icon16", title: "title16"},
-    {id: 17, icon: "icon17", title: "title17"},
-    {id: 18, icon: "icon18", title: "title18"}
-  ];
-
-  let displayedRestaurants = restaurants.slice(0, loadCounter);
+  useEffect(() => {
+    fetch("http://localhost:5000/getmenus")
+    .then(response => response.json())
+    .then(data => {console.log(data); setMenusAPI(data)});
+  }, []);
 
   return (
     <div id="restaurants">
         <div id="restaurants-container">
             <input type="search" placeholder="Search by Name & Category..." />
             <div id="restaurants-grid">
-              {displayedRestaurants.map(restaurant => (
-                  <Link to={"/menu"}>
-                    <div key={restaurant.id} className="restaurant">
-                      <i>{restaurant.icon}</i>
-                      <p>{restaurant.title}</p>
+              {menusAPI.map(menu => (
+                  <Link key={menu.id} to={"/menu"}>
+                    <div id={menu.id} className="restaurant">
+                      <i>{menu.restaurant}</i>
+                      <p>{menu.cuisine}</p>
                     </div>
                   </Link>
               ))}
             </div>
-            <button onClick={() => setLoadCounter(previous => previous + 8)}>Load More!</button>
+            <button>Load More!</button>
         </div>
     </div>
   )
